abb065d2a03145f2313cee42569a11cd88988dcd
alexi <alexi@LAPTOP-D8GKQUQP.home-life.hub>
2020-08-21 11:50:36 +0200

    Added Json parsing

--- src/main/java/it/univpm/esameOOP/utility/getFileFromJSON.java
+++ src/main/java/it/univpm/esameOOP/utility/getFileFromJSON.java
@@ -2,16 +2,31 @@ package it.univpm.esameOOP.utility;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
-import org.json.simple.JSONValue;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import it.univpm.esameOOP.model.SharedFile;
 
 
 public class getFileFromJSON {
-
-		//public ArrayList<SharedFile> getFile(JSONArray jArray) {
-			
-		//}
+		getJSONFromURL json = new getJSONFromURL();
+		JSONArray array = (JSONArray)json.getJSON().get("links");
+		ArrayList<SharedFile> sharedLinks = new ArrayList<SharedFile>();
+		
+		public ArrayList<SharedFile> getFile() {
+			JSONObject obj = new JSONObject();
+			Iterator<?> it = array.iterator();
+			SharedFile file = new SharedFile();
+			while (it.hasNext()) {
+				obj = (JSONObject)it.next();
+				file.setName((String)obj.get("name"));
+				file.setPath((String)obj.get("path_lower"));
+				file.setType((String)obj.get("preview_type"));
+				String name = file.getName();
+				file.setExtension(name.substring(name.lastIndexOf(".")));
+				sharedLinks.add(file);
+			}
+			return sharedLinks;
+		}
 }
--- src/main/java/it/univpm/esameOOP/utility/getJSONFromURL.java
+++ src/main/java/it/univpm/esameOOP/utility/getJSONFromURL.java
@@ -1,55 +1,36 @@
 package it.univpm.esameOOP.utility;
 
 import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.FileReader;
 
-import java.net.URLConnection;
 import java.net.URL;
 import java.net.HttpURLConnection;
 
-import java.util.Scanner;
-
 public class getJSONFromURL {
-	String url = "https://api.dropboxapi.com/2/sharing/list_shared_links?authorization=Bearer ";
-	String tokenFile = "token.txt";
-	String data = "";
+	String url = "https://api.dropboxapi.com/2/sharing/list_shared_links";
+	String jsonString = "";
 	String line = "";
-	JSONArray json=null;
-	public JSONArray getJSONfromURL() {
-	//Read the token from token.txt and append it to the URL
-	try{
-		Scanner in = new Scanner(new BufferedReader(new FileReader(tokenFile)));
-		while (in.hasNext()) {
-			url += in.next();
-		}
-		System.out.println(url);
-		in.close();
-	}catch(
-	IOException e)
-	{
-		System.out.println("Error generating the URL!");
-		e.printStackTrace();
-	}
-	
+	JSONObject json=null;
+	public JSONObject getJSON() {
+		
 	try {
 		
-		URLConnection openConnection = new URL(url).openConnection();
-		openConnection.addRequestProperty("User-Agent",	"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
-		InputStream inp = openConnection.getInputStream();
+		HttpURLConnection urlConnection = (HttpURLConnection) new URL(url).openConnection();
+		urlConnection.setRequestMethod("POST");
+		urlConnection.setRequestProperty("Authorization", "Bearer F7ARYGDyGpQAAAAAAAAAAVLCKzXKrdhTU9zzByXHePE0sP8g1XFYf059v5Fkmwxp");
+		urlConnection.addRequestProperty("User-Agent",	"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+		InputStream input = urlConnection.getInputStream();
 		try {
 			
-			BufferedReader buff = new BufferedReader(new InputStreamReader(inp, "UTF-8"));
+			BufferedReader buff = new BufferedReader(new InputStreamReader(input));
 			
 			while((line=buff.readLine()) != null) {
-				data += line;
+				jsonString += line;
 			}
 		}
 		catch(Exception e) {
@@ -57,11 +38,10 @@ public class getJSONFromURL {
 			e.printStackTrace();
 		}
 		finally {
-			inp.close();
+			input.close();
 		}
-		System.out.println(data);
 		try {
-			json = (JSONArray)JSONValue.parseWithException(data);
+			json = (JSONObject) JSONValue.parseWithException(jsonString);
 		}
 		catch(ParseException e) {
 			System.out.println ("Parsing error!");
@@ -72,7 +52,7 @@ public class getJSONFromURL {
 		System.out.println("Error!");
 		e.printStackTrace();
 	}
-	System.out.println(json);
+	//System.out.println(json);
 	return json;
 	}
 }
