eb249e8e74b83a3f84fe56ff45f7491fccdf74da
Fabrizio Pilone <s1086830@studenti.univpm.it>
2020-09-10 11:20:05 +0200

    Added sequence diagrams, updated use case diagram

--- src/main/java/it/univpm/esameOOP/controller/Controller.java
+++ src/main/java/it/univpm/esameOOP/controller/Controller.java
@@ -1,8 +1,6 @@
 package it.univpm.esameOOP.controller;
 
 import org.json.simple.JSONObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
